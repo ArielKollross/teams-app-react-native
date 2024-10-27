19af6b5ccc1c1ad4330c51129616a35d30af6427
arielkollross <arielkollross@gmail.com>
2024-10-26 22:30:09 -0300

    styles: styleds configs

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator } from 'react-native'
+import { ThemeProvider } from 'styled-components'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
-import { Groups } from './scr/screens/Groups';
+import theme from '@theme/index'
+
+import { Groups } from '@screens/Groups';
 
 export default function App() {
+  const [fontsLoaded] =  useFonts({ Roboto_400Regular, Roboto_700Bold});
+
   return (
-        <Groups />
+      <ThemeProvider theme={theme}>
+        { fontsLoaded 
+          ? <Groups /> 
+          : <ActivityIndicator/>  
+        }
+      </ThemeProvider>
   );
 }
